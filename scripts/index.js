802a2e14479220634f2d64cc51e4a7126f770242
AlexPozdniak <sotich911@gmail.com>
2023-03-06 15:35:39 +0300

    Mesto Russia
    Mesto Russia

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_job');
+let name = document.querySelector('.profile__heading');
+let job = document.querySelector('.profile__subheading');
+function openPopup () {
+    popup.classList.add('popup_opened');
+}
+function closePopup () {
+popup.classList.remove('popup_opened');
+}
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+
+    let nameInputValue = nameInput.value;
+    let jobInputValue = jobInput.value;
+    name.textContent = nameInputValue;
+    job.textContent = jobInputValue;
+closePopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
