b9b3869b91704387fce2a708fb9be9fb73aa9c69
AlexPozdniak <sotich911@gmail.com>
2023-03-09 21:00:07 +0300

    Mesto-Russia
    Mesto-Russia

--- scripts/index.js
+++ scripts/index.js
@@ -8,14 +8,12 @@ let name = document.querySelector('.profile__heading');
 let job = document.querySelector('.profile__subheading');
 function openPopup () {
     popup.classList.add('popup_opened');
-    document.querySelector('.popup__text_name').value = document.querySelector('.profile__heading').textContent;
-    document.querySelector('.popup__text_profession').value = document.querySelector('.profile__profession').textContent;
+    nameInput.value = name.textContent;
 }
 
 function closePopup () {
     popup.classList.remove('popup_opened');
-document.querySelector('.popup__text_name').value = document.querySelector('.profile__heading').textContent;
-    document.querySelector('.popup__text_profession').value = document.querySelector('.profile__profession').textContent;
+
 }
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
@@ -23,10 +21,8 @@ closeButton.addEventListener('click', closePopup);
 function handleFormSubmit (evt) {
     evt.preventDefault();
 
-    nameInputValue = nameInput.value;
-    jobInputValue = jobInput.value;
-    name.textContent = nameInputValue;
-    job.textContent = jobInputValue;
+    name.textContent = nameInput.value;
+    job.textContent = jobInput.value;
     closePopup();
 }
 
